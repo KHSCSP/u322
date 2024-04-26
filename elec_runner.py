c3609bc28cb204131a74b3dac1ca814a44465aac
heyscottn <heyscottn@gmail.com>
2024-04-26 09:30:44 -0500

    mm

---
+++ elec_runner.py
@@ -0,0 +1,44 @@
+# a322_electricity_trends.py
+# This program uses the pandas module to load a 3-dimensional data sheet into a pandas DataFrame object
+# Then it will use the matplotlib module to plot comparative line graphs 
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# choose countries of interest
+my_countries = ['United States', 'Zimbabwe','Cuba', 'Caribbean small states', "Cameroon", "Burundi"]
+
+# Load in the data with read_csv()
+df = pd.read_csv("u322/elec_access_data.csv", header=0)    # header=0 means there is a header in row 0
+
+# get a list unique countries
+unique_countries = df['Entity'].unique()
+
+
+# TIP:
+# make a list of markers, line types, and colors
+# and custom line types
+# when configuring the plot, use random.choice to get from your list
+
+
+# Plot the data on a line graph
+for c in unique_countries:
+    if c in my_countries:
+      
+        # match country to one of our we want to look at and get a list of years
+        years = df[df['Entity'] == c]['Year']
+        # This line of code is setting years equal to a list of years for a specific country
+        # more info about this in the book
+
+        # match country to one of our we want to look at and get a list of electriciy values
+        sum_elec = df[df['Entity'] == c]['Access']
+
+        # add each countries data to the graph
+        plt.plot(years, sum_elec, label=c, marker=random.choice(markers), linestyle=random.choice(linetypes))
+
+  
+plt.ylabel('Percentage of Country Population')
+plt.xlabel('Year')
+plt.title('Percent of Population with Access to Electricity')
+plt.legend()
+plt.show()
--- explore.py
+++ explore.py
@@ -1,12 +1,10 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-
-# TODO uncomment the print statements, one at a time
-# run the code each time
 
 
 # the main difference between this activity and the last one:
 # 3.2.1 graphs all data
-# 3.2.2 chooses a portion of the data
+# 3.2.2 chooses a portion of the data and groups data together
 
 
 
@@ -14,26 +12,27 @@ import pandas as pd
 # 1) file input old school vs pandas
 # 2) make a list without duplicates (list vs pandas)
 # 3) make a list of just United States items (list vs pandas)
+# 4) plot the data for the US (year versus access)
 
 
 
 # 1) read the data from the file
-f = open("u322/elec_access_data.csv")
+f = open("u32SOLNS/u322SOLNS/elec_access_data.csv")
 my_list = []
 for line in f:
     temp = line.strip()
     my_list.append(temp.split(','))   # append each row as a list
 f.close()
-# print("\na slice:", my_list[0:5])
+print("\nlook at a slice:", my_list[0:5])
+# ^^ comment out when ready to move on
 
 
 
 # 2) make a list of countries, remove duplicates with 'set'
 countries = []
-for row in my_list[1:]:
-    countries.append(row[0])
+# TODO
 
-countries = list(set(countries))
+
 # print("\nno duplicates using 'set':", countries)
 # print("length of countries, no duplicates:", len(countries))
 
@@ -44,35 +43,41 @@ us_list = []
 for row in my_list[1:]:
     if row[0] == 'United States':
         us_list.append(row)
-# print("\nlist of US items:", us_list)
+print("\nlist of US items:", us_list)
 
 
 
 # ---------------------------------------
 print("\n\n---- pandas ----")
 # 1) read the data
-df = pd.read_csv("u32SOLNS/u322SOLNS/elec_access_data.csv", header=0)
-
+df = pd.read_csv("u322/elec_access_data.csv", header=0)
 
 # 2) no duplicates
-countries = df['Entity'].unique()
+# TODO
+
 # print("\npandas no duplicates:", countries)
 
 
 
 # 3) data frame of entities == 'United States'
-us_list = df[df['Entity'] == 'United States']
+# this is still a bit of magic to me...
+# TODO
+
 # NOTE: When using [], the result is a Pandas DataFrame.
 # print("\npandas data frame of US:\n", us_list)
-# print(type(us_list))
 
 
-# further experiments
+
+
+# 4) plotting data for us: years and access
 # create a DataFrame of all the rows where the country is equal to United States, get just the years
-us_list = df[df['Entity'] == 'United States']['Year']
-# print("\npandas data frame of US years only:\n", us_list)
-# print(type(us_list))
+# TODO
 
+# print("\n maybe look at this pandas data frame:\n", us_access)
+
+# plt.plot(us_years, us_access)
+# plt.show()
+# ^^ obviously uncomment those to see the graph
 
 
 
